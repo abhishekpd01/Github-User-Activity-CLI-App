9015e70184bf512c1a0a10b1a8a3aea4e288e0a1
Abhishek Prasad <prasadabhishek550@gmail.com>
2025-11-17 14:44:49 +0530

    Implemented Caching logic

---
+++ index.js
@@ -0,0 +1,93 @@
+#!/user/bin/env node
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// --- Configuration ---
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Defining Cache Settings
+const CACHE_DIR = path.join(__dirname, '.cache');
+const CACHE_DURATION_MS = 30 * 60 * 1000;   // 30 min
+
+const userName = process.argv[2];
+
+if(!userName) {
+    console.log('Usage: github-activity <username>');
+    process.exit(1);
+}
+
+function getCachedData(username) {
+    /**
+     * Tries to get fresh cached data for a user.
+     * Returns parsed JSON data if found and fresh, otherwise null.
+    */
+    const cacheFilePath = path.join(CACHE_DIR, `${username}.json`);
+
+    try {
+        // Check if file exists
+        if(!fs.existsSync(cacheFilePath)) {
+            return null;
+        }
+
+        // Check if file is expired.
+        const stats = fs.statSync(cacheFilePath);
+        const fileAgeMs = Date.now() - stats.mtime.getTime()
+        if(fileAgeMs > CACHE_DURATION_MS) {
+            console.log('Cache is Stale');
+            return null;
+        }
+
+        // Read and Parse the file
+        console.log('Using Cached Data');
+        const data = fs.readFileSync(cacheFilePath, "utf-8");
+        return JSON.parse(data);
+
+    } catch (error) {
+        console.error('Error reading cache: ', error);
+        return null;
+    }
+}
+
+function setCache(username, data) {
+    /**
+    * Writes data to the cache for a specific user.
+    */
+
+    const cacheFilePath = path.join(CACHE_DIR, `${username}.json`);
+
+    try {
+        // Ensure the cache directory exists
+        fs.mkdirSync(CACHE_DIR, { recursive: true });
+
+        // Write the file
+        fs.writeFileSync(cacheFilePath, JSON.stringify(data, null, 2));
+        console.log('Cache Saved!');
+    } catch (error) {
+        console.error('Error writing to cache: ', error.message);
+    }
+}
+
+function main() {
+    // Try to get data from cache
+    const cachedData = getCachedData(userName);
+
+    if(cachedData) {
+        // If cache is valid, display it
+        console.log('--- Activity (from cache) ---');
+        console.log(cachedData);
+        return ;
+    }
+
+    // If cache is empty or stale, fetch from API.
+    console.log(`Fetching new data for ${userName}`);
+    // This is where you'll put your 'https.get' call (Phase 4)
+    // Inside your API call's 'end' event, you will:
+    //    const freshData = JSON.parse(rawData);
+    //    setCachedData(userName, freshData); // Save the new data
+    //    displayActivity(freshData); // Display the new data
+}
+
+main();
